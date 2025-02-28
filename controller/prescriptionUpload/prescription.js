4fcd62f57e393eea47b6ceaec2dbd183b54477ae
Equipped <93303641+InFutiveDev@users.noreply.github.com>
2025-02-28 13:20:59 +0530

    update firebase key

--- controller/prescriptionUpload/prescription.js
+++ controller/prescriptionUpload/prescription.js
@@ -75,113 +75,113 @@ const addprescription = async (req, res) => {
 
     const uploadTime = saveprescription.createdAt;
 
-    let smsPayload = {
-      message: `Thank you for sharing your prescription with us. We have registered your query/concern under Case ID: ${saveprescription.caseId}. Our Team will contact you soon. City X-Ray and Scan Clinic`,
-      mobile,
-    };
-    sendMsg(smsPayload);
+    // let smsPayload = {
+    //   message: `Thank you for sharing your prescription with us. We have registered your query/concern under Case ID: ${saveprescription.caseId}. Our Team will contact you soon. City X-Ray and Scan Clinic`,
+    //   mobile,
+    // };
+    // sendMsg(smsPayload);
 
-    const htmlTemplate = `
-       <!DOCTYPE html>
-<html>
-  <head>
-    <meta http-equiv="content-type" content="text/html; charset=windows-1252">
-    <style>
-      /* Reset styles for email clients */
-      img {
-        display: block;
-        border: 0;
-        outline: none;
-        text-decoration: none;
-      }
-      table {
-        border-spacing: 0;
-        border-collapse: collapse;
-      }
-      p {
-        margin: 0;
-        padding: 0;
-      }
-      a {
-        color: inherit;
-        text-decoration: none;
-      }
-    </style>
-  </head>
-  <body>
-    <table style="border: 10px solid #DD861F;" align="center" bgcolor="#ffffff" border="0" cellpadding="0" cellspacing="0" width="650">
-      <tbody>
-        <!-- Header Section -->
-        <tr>
-          <td align="left" valign="top">
-            <table style="border-bottom: 1px solid #cccccc;" border="0" cellpadding="0" cellspacing="0" width="650">
-              <tbody>
-                <tr>
-                  <td style="padding: 10px; background-color: #fcc42c;" align="left" valign="middle" width="275">
-                    <img
-                      alt="City X-Ray Clinic"
-                      src="https://www.cityxrayclinic.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FappLogo.e921700e.png&w=750&q=100"
-                      width="100"
-                      height="100"
-                      style="width: 100px; height: 40px; max-width: 100px; max-height: 100px;"
-                    />
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </td>
-        </tr>
+//     const htmlTemplate = `
+//        <!DOCTYPE html>
+// <html>
+//   <head>
+//     <meta http-equiv="content-type" content="text/html; charset=windows-1252">
+//     <style>
+//       /* Reset styles for email clients */
+//       img {
+//         display: block;
+//         border: 0;
+//         outline: none;
+//         text-decoration: none;
+//       }
+//       table {
+//         border-spacing: 0;
+//         border-collapse: collapse;
+//       }
+//       p {
+//         margin: 0;
+//         padding: 0;
+//       }
+//       a {
+//         color: inherit;
+//         text-decoration: none;
+//       }
+//     </style>
+//   </head>
+//   <body>
+//     <table style="border: 10px solid #DD861F;" align="center" bgcolor="#ffffff" border="0" cellpadding="0" cellspacing="0" width="650">
+//       <tbody>
+//         <!-- Header Section -->
+//         <tr>
+//           <td align="left" valign="top">
+//             <table style="border-bottom: 1px solid #cccccc;" border="0" cellpadding="0" cellspacing="0" width="650">
+//               <tbody>
+//                 <tr>
+//                   <td style="padding: 10px; background-color: #fcc42c;" align="left" valign="middle" width="275">
+//                     <img
+//                       alt="City X-Ray Clinic"
+//                       src="https://www.cityxrayclinic.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FappLogo.e921700e.png&w=750&q=100"
+//                       width="100"
+//                       height="100"
+//                       style="width: 100px; height: 40px; max-width: 100px; max-height: 100px;"
+//                     />
+//                   </td>
+//                 </tr>
+//               </tbody>
+//             </table>
+//           </td>
+//         </tr>
 
-        <!-- Body Section -->
-        <tr>
-          <td align="left" valign="top">
-            <table border="0" cellpadding="0" cellspacing="0" width="650">
-              <tbody>
-                <tr>
-                  <td colspan="3" height="30" width="100%">&nbsp;</td>
-                </tr>
-                <tr>
-                  <td width="30">&nbsp;</td>
-                  <td style="font-size: 13px; color: #333333; font-family: Arial, sans-serif;" width="590">
-                    <p style="font-size: 18px; color: #DD861F; margin-bottom: 0.5em;">&nbsp;</p>
-                    <p style="font-size: 13px; font-weight: bold;">
-                      Thanks for submitting your enquiry with us. We will get back to you soon.
-                    </p>
-                    <p><b>Name:</b> ${userName}</p>
-                    <p><b>Email:</b> ${email}</p>
-                    <p><b>Phone Number:</b> ${mobile}</p>
-                    <p><b>Prescription:</b> ${uploads}</p>
-                    <p><b>Prescription Id:</b> ${saveprescription.caseId}</p>
-                    <p><b>Feedback Time:</b> ${uploadTime}</p>
-                    <p>
-                      For any enquiry, call us on
-                      <a href="tel:011-4725-2000" style="color: #DD861F; text-decoration: none;">+91-011-4725-2000</a>
-                    </p>
-                    <p><b>Thank you,</b></p>
-                    <p><b>City X-Ray & Scan Clinic Pvt Ltd</b></p>
-                  </td>
-                  <td width="30">&nbsp;</td>
-                </tr>
-                <tr>
-                  <td colspan="3" height="30" width="100%">&nbsp;</td>
-                </tr>
-              </tbody>
-            </table>
-          </td>
-        </tr>
-      </tbody>
-    </table>
-  </body>
-</html>
-        `;
+//         <!-- Body Section -->
+//         <tr>
+//           <td align="left" valign="top">
+//             <table border="0" cellpadding="0" cellspacing="0" width="650">
+//               <tbody>
+//                 <tr>
+//                   <td colspan="3" height="30" width="100%">&nbsp;</td>
+//                 </tr>
+//                 <tr>
+//                   <td width="30">&nbsp;</td>
+//                   <td style="font-size: 13px; color: #333333; font-family: Arial, sans-serif;" width="590">
+//                     <p style="font-size: 18px; color: #DD861F; margin-bottom: 0.5em;">&nbsp;</p>
+//                     <p style="font-size: 13px; font-weight: bold;">
+//                       Thanks for submitting your enquiry with us. We will get back to you soon.
+//                     </p>
+//                     <p><b>Name:</b> ${userName}</p>
+//                     <p><b>Email:</b> ${email}</p>
+//                     <p><b>Phone Number:</b> ${mobile}</p>
+//                     <p><b>Prescription:</b> ${uploads}</p>
+//                     <p><b>Prescription Id:</b> ${saveprescription.caseId}</p>
+//                     <p><b>Feedback Time:</b> ${uploadTime}</p>
+//                     <p>
+//                       For any enquiry, call us on
+//                       <a href="tel:011-4725-2000" style="color: #DD861F; text-decoration: none;">+91-011-4725-2000</a>
+//                     </p>
+//                     <p><b>Thank you,</b></p>
+//                     <p><b>City X-Ray & Scan Clinic Pvt Ltd</b></p>
+//                   </td>
+//                   <td width="30">&nbsp;</td>
+//                 </tr>
+//                 <tr>
+//                   <td colspan="3" height="30" width="100%">&nbsp;</td>
+//                 </tr>
+//               </tbody>
+//             </table>
+//           </td>
+//         </tr>
+//       </tbody>
+//     </table>
+//   </body>
+// </html>
+//         `;
 
-    sendEmail({
-      subject: `CXR(Prescription)-${mobile}`,
-      html: htmlTemplate.replace(/{{([^}]+)}}/g, (match, key) => data[key]),
-      to: process.env.TOMAIL,
-      from: process.env.EMAIL,
-      cc: process.env.CC,
-    });
+//     sendEmail({
+//       subject: `CXR(Prescription)-${mobile}`,
+//       html: htmlTemplate.replace(/{{([^}]+)}}/g, (match, key) => data[key]),
+//       to: process.env.TOMAIL,
+//       from: process.env.EMAIL,
+//       cc: process.env.CC,
+//     });
     if (!saveprescription) {
       const obj = {
         res,
